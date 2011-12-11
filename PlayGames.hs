6c89b4c39b8562bb192296b9175dda022870509f
Nicu Ionita <nicu.ionita@acons.at>
2011-12-11 14:37:02 +0100

    Utility to play a game with random map and players

--- PlayGames.hs
+++ PlayGames.hs
@@ -1,10 +1,15 @@
 module Main where
 
+import Control.Applicative ((<$>))
+import Control.Monad (filterM)
 import Data.List
 import Data.Maybe (mapMaybe, fromJust)
 import qualified Data.Map as M
 import System.Environment (getArgs)
 import System.Directory
+import System.FilePath
+import System.Cmd
+import System.Random
 
 import Config
 
@@ -29,14 +34,16 @@ main = do
            let kmiss = checkConfig conf
            if null kmiss
                then do
-                   let cf = configRecord conf
+                   let cf = configRecord $ resolveConfig conf
                    runWith cf opts
                else putStrLn $ "Keys are missing in " ++ cfile ++ ": "
                                ++ concat (intersperse ", " kmiss)
 
 checkConfig :: Config -> [String]
-checkConfig conf = mapMaybe check ["Root", "Play", "LogDir", "Maps", "Engine" ]
-    where check k = if keyVal conf k == Nothing then Just k else Nothing
+checkConfig conf = mapMaybe checkV ["Root", "Play", "LogDir", "Maps", "Engine"]
+                ++ mapMaybe checkD ["Engines"]
+    where checkV k = if keyVal conf k == Nothing then Just k else Nothing
+          checkD k = if keyMap conf k == Nothing then Just k else Nothing
 
 configRecord :: Config -> ConfigRecord
 configRecord conf = CF {
@@ -47,23 +54,60 @@ configRecord conf = CF {
     cfMaps = fromJust $ keyVal conf "Maps",
     cfPars = fromJust $ keyVal conf "Pars",
     cfEngine = fromJust $ keyVal conf "Engine",
-    cfEngines = case keyMap conf "Engines" of
-                    Just m -> m
-                    _      -> M.empty
+    cfEngines = fromJust $ keyMap conf "Engines"
     }
 
 runWith cf opts = do
     maps <- getMaps cf
-    mapM_ putStrLn maps
-    -- putStrLn "CmdLine:"
-    -- let cmd = 
+    r <- randomRIO (0, length maps - 1)
+    let mymap = maps!!r
+        pl = fromJust $ playerNum mymap
+    putStrLn $ "Choose map " ++ mymap ++ " with " ++ show pl ++ " players"
+    let logs = "--log_dir " ++ (cfRoot cf </> cfLogDir cf)
+        trns = "--turns " ++ show (cfTurns cf)
+        mapf = "--map_file " ++ mymap
+    engs <- map quote <$> randomEngines (pl-1) (M.elems $ cfEngines cf)
+    let cmd = unwords $ [ cfPlay cf, logs, trns, cfPars cf, mapf, cfEngine cf] ++ engs
+    putStrLn "CmdLine:"
+    putStrLn cmd
+    system cmd
+    return ()
 
 getMaps :: ConfigRecord -> IO [String]
 getMaps cf = do
-    -- case keyVal conf 
-    cwd <- getCurrentDirectory
-    -- setCurrentDirectory mdir
-    setCurrentDirectory cwd
-    return []
+    let mdir = cfRoot cf </> cfMaps cf
+    msdirs <- map (mdir </>) <$> filter ((/= '.') . head) <$> onlyDirs mdir
+    -- putStrLn "Map subdirs:"
+    -- mapM_ putStrLn msdirs
+    maps   <- concat <$> mapM getMapFiles msdirs
+    -- putStrLn "Maps:"
+    -- mapM_ putStrLn maps
+    return maps
+
+onlyDirs path = getDirectoryContents path >>= filterM (\f -> doesDirectoryExist $ path </> f)
+
+getMapFiles dir
+    = map (dir </>) <$> filter ((/= Nothing) . playerNum) <$> filter (".map" `isSuffixOf`)
+         <$> getDirectoryContents dir >>= filterM (\f -> doesFileExist $ dir </> f)
+
+playerNum :: String -> Maybe Int
+playerNum s = go $ infix0 ++ infix1
+    where infix1 = [ ("_p0" ++ show i ++ "_", i)    | i <- [2..9]]
+                ++ [ ("_p1" ++ show i ++ "_", i+10) | i <- [0..2]]
+          infix0 = [ ("_0" ++ show i ++ "p_", i)    | i <- [2..9]]
+                ++ [ ("_1" ++ show i ++ "p_", i+10) | i <- [0..2]]
+          go [] = Nothing
+          go ((ifi, k):is) = if ifi `isInfixOf` s then Just k else go is
+
+randomEngines :: Int -> [String] -> IO [String]
+randomEngines n es = go [] n
+    where l = length es - 1
+          go acc 0 = return acc
+          go acc k = do
+             r <- randomRIO (0, l)
+             go ((es!!r) : acc) (k-1)
+
+quote :: String -> String
+quote x = '"' : x ++ ['"']
 
 -- vim: set expandtab:
